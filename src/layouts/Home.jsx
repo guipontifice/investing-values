a85b22bec03186c042ebf54c69205af13ee5ac6f
Guilherme <guilhermepontifice.gp@gmail.com>
2024-02-27 10:36:41 -0300

    add: filter features

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 function Home() {
   const [stocks, setStocks] = useState([]);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,9 +14,16 @@ function Home() {
   }, []);
 
   return (
-    <div className='flex flex-row w-full'>
-      <div className='flex flex-col p-3'>
-        <div className='grid grid-cols-9 gap-4 items-center'>
+    <div className='flex flex-row w-full justify-center'>
+
+      <div className='flex flex-col p-3 '>
+        <input
+          type='text'
+          placeholder='Pesquisar Ação'
+          className='flex w-full mb-5 border-2 p-1'
+          onChange={(event) => setQuery(event.target.value)} />
+
+        <div className='grid grid-cols-9 gap-4 items-center border-2 mb-3'>
           <h2 className='col-span-2 mx-2'>Name</h2>
           <p className='m-2'>Valor</p>
           <p className='m-2'>Variação</p>
@@ -26,11 +34,13 @@ function Home() {
           <p className='m-2'>Anterior</p>
         </div>
         {stocks ? (
-          stocks.map((stock, index) => (
-            <div key={index} className='flex border-2'>
+          stocks.filter(stock => 
+            stock.name.toLowerCase().includes(query)
+          ).map((stock, index) => (
+            <div key={index} className='flex border-2 mb-1 text-xs'>
               <div key={index} className='w-full grid grid-cols-9 gap-3 items-center'>
                 <h2 className='col-span-2 mx-2'>{stock.name}</h2>
-                  <p className='align-middle flex flex-col m-5'>{stock.value}</p>
+                <p className='align-middle flex flex-col m-5'>{stock.value}</p>
                 <p className={`m-5 ${((1 / (stock.variation * 0)) === 1 / 0 ? 'text-black' : 'text-red')}`}>
                   {stock.variation}
                 </p>
